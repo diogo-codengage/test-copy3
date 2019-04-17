e5e961b534021def817cfed61b0bdee980b0861e
Vinicius Lohmann Kieling <c.viniciuslk@gmail.com>
2019-04-17 13:54:32 -0300

    Atualiza dependencias do repositorio de exemplo packages/es-demo

--- packages/es-demo/src/App.js
+++ packages/es-demo/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { ESButton } from "sanar-ui";
+import ESButton from "sanar-ui/dist/Components/Atoms/Button";
 
 class App extends Component {
 	render() {
