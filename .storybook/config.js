79e7a43ad8c27843aeb08b5651a4a0445441345f
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-04-17 14:18:03 -0300

    Corrige utilizacao do storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,7 +6,7 @@ import "./styles.less";
 addDecorator(story => <div style={{ padding: 20 }}>{story()}</div>);
 
 // automatically import all files ending in *.stories.js
-const req = require.context("../packages/sanar-ui", true, /\.stories\.js$/);
+const req = require.context("../packages/sanar-ui/src", true, /\.stories\.js$/);
 function loadStories() {
 	req.keys().forEach(filename => req(filename));
 }
