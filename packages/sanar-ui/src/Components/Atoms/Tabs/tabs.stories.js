5e3d5183296d9dc20ec235436004f063febd6514
diogobiz <diogo@codengage.com>
2019-04-22 14:16:10 -0300

    Set propTypes tabs

--- packages/sanar-ui/src/Components/Atoms/Tabs/tabs.stories.js
+++ packages/sanar-ui/src/Components/Atoms/Tabs/tabs.stories.js
@@ -1,14 +1,41 @@
-import React, { useState } from "react";
-import { storiesOf } from "@storybook/react";
+import React, { useState } from 'react'
+import { storiesOf } from '@storybook/react'
+import { select, boolean } from '@storybook/addon-knobs'
 
-import ESTabs from "./Tabs";
-import ESTabPane from "./TabPane";
+import ESTabs from './Tabs'
+import ESTabPane from './TabPane'
 
-storiesOf("Atoms.Tabs", module)
-    .add("Simple", () => (
-        <ESTabs defaultActiveKey="1">
-            <ESTabPane tab="Dados cadastrais" key="1">Content Dados cadastrais</ESTabPane>
-            <ESTabPane tab="Alterar senha" key="2">Content Alterar senha</ESTabPane>
-        </ESTabs>
-    ))
-    
+const sizeOptions = {
+    Default: 'default',
+    Large: 'large',
+    Small: 'small'
+}
+
+const typeOptions = {
+    Line: 'line',
+    Card: 'card',
+    EditableCard: 'editable-card'
+}
+
+const positionOptions = {
+    Top: 'top',
+    Right: 'right',
+    Bottom: 'bottom',
+    Left: 'left'
+}
+
+storiesOf('Atoms.Tabs', module).add('Simple', () => (
+    <ESTabs
+        defaultActiveKey='1'
+        size={select('Size', sizeOptions, 'default')}
+        type={select('Type', typeOptions, 'line')}
+        tabPosition={select('Tab position', positionOptions, 'top')}
+    >
+        <ESTabPane tab='Dados cadastrais' key='1'>
+            Content Dados cadastrais
+        </ESTabPane>
+        <ESTabPane tab='Alterar senha' key='2'>
+            Content Alterar senha
+        </ESTabPane>
+    </ESTabs>
+))
