9464db944d5b70b79d7d22c041859e41ffd87cc1
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-04-29 13:38:27 -0300

    feat(e-sanar): add import to style file in App.js

--- packages/e-sanar/src/App.js
+++ packages/e-sanar/src/App.js
@@ -9,6 +9,8 @@ import PasswordRecoveryPage from './Pages/Auth/PasswordRecovery'
 import SignupPage from './Pages/Auth/Signup'
 import { CoursePage } from './Pages/Portal/Course'
 
+import './App.less'
+
 const client = new ApolloClient({
     uri: 'https://48p1r2roz4.sse.codesandbox.io'
 })
@@ -20,7 +22,10 @@ function ESRouter() {
                 <Route path='/' component={HomePage} />
                 <Route path='/signin' component={SigninPage} />
                 <Route path='/signup' component={SignupPage} />
-                <Route path='/password-recovery' component={PasswordRecoveryPage} />
+                <Route
+                    path='/password-recovery'
+                    component={PasswordRecoveryPage}
+                />
                 <PrivateRoute path='/course' component={CoursePage} />
             </Router>
         </ApolloProvider>
