2368030927546ee54ddabcb0aca7618cf4cf7270
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-04-30 17:35:11 -0300

    refactor(e-sanar): user container layout on Interactions and Lives

--- packages/e-sanar/src/Pages/Portal/Course/Interactions.jsx
+++ packages/e-sanar/src/Pages/Portal/Course/Interactions.jsx
@@ -12,6 +12,7 @@ import ESBadge from 'sanar-ui/dist/Components/Atoms/Badge'
 import { ESRow, ESCol } from 'sanar-ui/dist/Components/Atoms/Grid'
 
 import Mock from './mock.json'
+import { SANPortalPagesContainer } from '../Layout/index.js'
 
 const SANInteractions = () => {
     const { t } = useTranslation()
@@ -20,76 +21,78 @@ const SANInteractions = () => {
     } = Mock
 
     return (
-        <ESRow type='flex' className='interactions' gutter={24}>
-            <ESCol
-                xs={24}
-                sm={24}
-                md={12}
-                alignSelf='stretch'
-                className='interactions__column'
-            >
-                <ESSessionTitle
-                    title={t('courseDetails.recentCommentsTitle')}
-                    subtitle={t('courseDetails.recentCommentsSubtitle')}
-                />
-                <ESCard
-                    actions={[
-                        <ESButton clear ghost type='primary' fontSize={12}>
-                            {t('courseDetails.recentCommentsButton')}
-                            <ESBadge count={31} />
-                        </ESButton>
-                    ]}
+        <SANPortalPagesContainer>
+            <ESRow type='flex' className='interactions' gutter={24}>
+                <ESCol
+                    xs={24}
+                    sm={24}
+                    md={12}
+                    alignSelf='stretch'
+                    className='interactions__column'
                 >
-                    <ESListView>
-                        <ESListViewItem
-                            avatar='https://www.domalberto.edu.br/wp-content/uploads/2017/02/joao.png'
-                            avatarSize='large'
-                            title='Proin ex ipsum, facilisis id tincidunt sed, vulputate in lacus…h'
-                            description='Paula Aguiar da Silva'
-                            counter={0}
-                        />
-                        <ESListViewItem
-                            avatar='https://www.domalberto.edu.br/wp-content/uploads/2017/02/joao.png'
-                            avatarSize='large'
-                            title='Donec pharetra faucibus leo, vitae vestibulum leo sceleris…h'
-                            description='José João Junior'
-                            counter={0}
-                        />
-                    </ESListView>
-                </ESCard>
-            </ESCol>
-            <ESCol xs={24} sm={24} md={12} alignSelf='stretch'>
-                <ESSessionTitle
-                    title={t('courseDetails.recentlySavedTitle')}
-                    subtitle={t('courseDetails.recentlySavedSubtitle')}
-                />
-                <ESCard
-                    actions={[
-                        <ESButton clear ghost type='primary' fontSize={12}>
-                            {t('courseDetails.recentlySavedButton')}
-                            <ESBadge count={5} />
-                        </ESButton>
-                    ]}
-                >
-                    <ESListView>
-                        <ESListViewItem
-                            avatar='https://www.domalberto.edu.br/wp-content/uploads/2017/02/joao.png'
-                            avatarSize='large'
-                            title='Legislação do Sistema Único de Saúde e Saúde Coletiva'
-                            description='Módulo EBSERH'
-                            category='AULA'
-                        />
-                        <ESListViewItem
-                            avatar='https://www.domalberto.edu.br/wp-content/uploads/2017/02/joao.png'
-                            avatarSize='large'
-                            title='Legislação do Sistema Único de Saúde e Saúde Coletiva'
-                            description='Módulo EBSERH'
-                            category='QUESTÃO'
-                        />
-                    </ESListView>
-                </ESCard>
-            </ESCol>
-        </ESRow>
+                    <ESSessionTitle
+                        title={t('courseDetails.recentCommentsTitle')}
+                        subtitle={t('courseDetails.recentCommentsSubtitle')}
+                    />
+                    <ESCard
+                        actions={[
+                            <ESButton clear ghost type='primary' fontSize={12}>
+                                {t('courseDetails.recentCommentsButton')}
+                                <ESBadge count={31} />
+                            </ESButton>
+                        ]}
+                    >
+                        <ESListView>
+                            <ESListViewItem
+                                avatar='https://www.domalberto.edu.br/wp-content/uploads/2017/02/joao.png'
+                                avatarSize='large'
+                                title='Proin ex ipsum, facilisis id tincidunt sed, vulputate in lacus…h'
+                                description='Paula Aguiar da Silva'
+                                counter={0}
+                            />
+                            <ESListViewItem
+                                avatar='https://www.domalberto.edu.br/wp-content/uploads/2017/02/joao.png'
+                                avatarSize='large'
+                                title='Donec pharetra faucibus leo, vitae vestibulum leo sceleris…h'
+                                description='José João Junior'
+                                counter={0}
+                            />
+                        </ESListView>
+                    </ESCard>
+                </ESCol>
+                <ESCol xs={24} sm={24} md={12} alignSelf='stretch'>
+                    <ESSessionTitle
+                        title={t('courseDetails.recentlySavedTitle')}
+                        subtitle={t('courseDetails.recentlySavedSubtitle')}
+                    />
+                    <ESCard
+                        actions={[
+                            <ESButton clear ghost type='primary' fontSize={12}>
+                                {t('courseDetails.recentlySavedButton')}
+                                <ESBadge count={5} />
+                            </ESButton>
+                        ]}
+                    >
+                        <ESListView>
+                            <ESListViewItem
+                                avatar='https://www.domalberto.edu.br/wp-content/uploads/2017/02/joao.png'
+                                avatarSize='large'
+                                title='Legislação do Sistema Único de Saúde e Saúde Coletiva'
+                                description='Módulo EBSERH'
+                                category='AULA'
+                            />
+                            <ESListViewItem
+                                avatar='https://www.domalberto.edu.br/wp-content/uploads/2017/02/joao.png'
+                                avatarSize='large'
+                                title='Legislação do Sistema Único de Saúde e Saúde Coletiva'
+                                description='Módulo EBSERH'
+                                category='QUESTÃO'
+                            />
+                        </ESListView>
+                    </ESCard>
+                </ESCol>
+            </ESRow>
+        </SANPortalPagesContainer>
     )
 }
 
--- packages/e-sanar/src/Pages/Portal/Course/Lives.jsx
+++ packages/e-sanar/src/Pages/Portal/Course/Lives.jsx
@@ -9,6 +9,7 @@ import ESSessionTitle from 'sanar-ui/dist/Components/Molecules/SessionTitle'
 import ESButton from 'sanar-ui/dist/Components/Atoms/Button'
 
 import Mock from './mock.json'
+import { SANPortalPagesContainer } from '../Layout/index.js'
 
 const SANLives = () => {
     const { t } = useTranslation()
@@ -21,25 +22,27 @@ const SANLives = () => {
     const date = format(lives[0].release_date, 'DD/MM/YYYY')
 
     return (
-        <div className='lives'>
-            <ESSessionTitle
-                title={t('courseDetails.recentCommentsTitle')}
-                subtitle={t('courseDetails.recentCommentsSubtitle')}
-            />
-            <ESLiveSection
-                videoSrc={lives[0].link}
-                description={lives[0].description}
-                title={lives[0].title}
-                date={date}
-                name={lives[0].professor.name}
-                courses={lives[0].professor.courses}
-                action={
-                    <ESButton size='small' block fontSize={12}>
-                        {t('courseDetails.seePreviousLives')}
-                    </ESButton>
-                }
-            />
-        </div>
+        <SANPortalPagesContainer>
+            <div className='lives'>
+                <ESSessionTitle
+                    title={t('courseDetails.recentCommentsTitle')}
+                    subtitle={t('courseDetails.recentCommentsSubtitle')}
+                />
+                <ESLiveSection
+                    videoSrc={lives[0].link}
+                    description={lives[0].description}
+                    title={lives[0].title}
+                    date={date}
+                    name={lives[0].professor.name}
+                    courses={lives[0].professor.courses}
+                    action={
+                        <ESButton size='small' block fontSize={12}>
+                            {t('courseDetails.seePreviousLives')}
+                        </ESButton>
+                    }
+                />
+            </div>
+        </SANPortalPagesContainer>
     )
 }
 
