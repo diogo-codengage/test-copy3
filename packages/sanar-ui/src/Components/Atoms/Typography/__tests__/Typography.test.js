8cb74707e4cc8921f84cae8f0b15390861783265
diogobiz <diogo@codengage.com>
2019-05-06 09:52:09 -0300

    fix(sanar-ui): set letter spacing in typhography

--- packages/sanar-ui/src/Components/Atoms/Typography/__tests__/Typography.test.js
+++ packages/sanar-ui/src/Components/Atoms/Typography/__tests__/Typography.test.js
@@ -1,15 +1,10 @@
-
 import React from 'react'
 import TestRenderer from 'react-test-renderer'
 
 import ESTypography from '../'
 
 it('renders correctly', () => {
-    const component = (
-        <ESTypography>
-            <h1>ESTypography</h1>
-        </ESTypography>
-    )
+    const component = <ESTypography>ESTypography</ESTypography>
 
     const tree = TestRenderer.create(component).toJSON()
     expect(tree).toMatchSnapshot()
