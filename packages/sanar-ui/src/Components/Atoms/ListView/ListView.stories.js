4e754a8f3fb0e88045a3539cc3d5b3a2aa7f7209
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-05-10 01:18:33 -0300

    refactor(sanar-ui): change ListViewItem to Atom

--- packages/sanar-ui/src/Components/Atoms/ListView/ListView.stories.js
+++ packages/sanar-ui/src/Components/Atoms/ListView/ListView.stories.js
@@ -1,24 +1,17 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import ListView from './ListView'
-import { text, number, boolean } from '@storybook/addon-knobs'
-import ESListViewItem from '../../Molecules/ListViewItem/ListViewItem'
+import { boolean } from '@storybook/addon-knobs'
+import ESListViewItem from './ListViewItem'
+import ESTypography from '../Typography'
 
 storiesOf('Atoms.ListView', module).add('Simple', () => (
     <ListView>
         <ESListViewItem
-            avatar={text(
-                'Avatar',
-                'https://www.domalberto.edu.br/wp-content/uploads/2017/02/joao.png'
-            )}
-            avatarSize='large'
-            title={text(
-                'Title',
-                'Blaaaa bla bla asfasd heuaeu haeuhauheu haueh auhe uh'
-            )}
-            description={text('Descripition', 'Descripition')}
-            counter={number('Counter', 0)}
+            avatar={boolean('Avatar', false)}
             loading={boolean('Loading', false)}
-        />
+        >
+            <ESTypography level={2}>Here is some content!</ESTypography>
+        </ESListViewItem>
     </ListView>
 ))
