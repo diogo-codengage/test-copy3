92b81ee3f7d7d653a6e71aeedbac4e148bbf730e
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-05-15 17:24:09 -0300

    fix(sanar-ui): fix goal position in progress component

--- packages/sanar-ui/src/Components/Molecules/ProgressBar/ProgressBar.jsx
+++ packages/sanar-ui/src/Components/Molecules/ProgressBar/ProgressBar.jsx
@@ -1,50 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Progress from 'antd/lib/progress'
-import Typography from 'antd/lib/typography'
-import { Row, Col } from 'antd'
 import classNames from 'classnames'
 
 import ESEvaIcon from '../../Atoms/EvaIcon'
 import ESCommonBadge from '../../Atoms/CommonBadge'
 import ESTypography from '../../Atoms/Typography'
 import ESProgress from '../../Atoms/Progress/Progress'
+import { ESRow, ESCol } from '../../Atoms/Grid'
 
 const ESProgressBar = ({ className, percent, title, limit }) => {
     const classes = classNames('es-progress-bar', className)
 
     return (
         <div className={classes}>
-            <Row type='flex' justify='space-between' align='middle'>
-                <Col>
+            <ESRow type='flex' justify='space-between' align='middle'>
+                <ESCol xs={12}>
                     <ESTypography level={6} className='es-progress-bar__title'>
                         {title}
                     </ESTypography>
-                </Col>
-                <Col>
-                    <Row
+                </ESCol>
+                <ESCol>
+                    <ESRow
                         gutter={16}
                         type='flex'
                         justify='space-between'
                         align='middle'
                     >
-                        <Col>
+                        <ESCol>
                             <ESCommonBadge
                                 count={percent}
                                 status='warning'
                                 suffix='%'
                             />
-                        </Col>
-                        <Col className='es-progress-bar__icon'>
+                        </ESCol>
+                        <ESCol className='es-progress-bar__icon'>
                             <ESEvaIcon
                                 size='small'
                                 name='award-outline'
-                                color={percent >= limit ? 'warning' : 'default'}
+                                EScoLESColor={
+                                    percent >= limit ? 'warning' : 'default'
+                                }
                             />
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
+                        </ESCol>
+                    </ESRow>
+                </ESCol>
+            </ESRow>
             <ESProgress
                 size='small'
                 status='warning'
