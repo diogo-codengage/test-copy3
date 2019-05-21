a6af2b8ae1053057cfc8207812df6a05e27533e7
diogobiz <diogo@codengage.com>
2019-05-21 10:02:23 -0300

    feat(e-sanar): add animation notifications

--- packages/e-sanar/src/Pages/Portal/Layout/Menu/Notifications.jsx
+++ packages/e-sanar/src/Pages/Portal/Layout/Menu/Notifications.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useTranslation } from 'react-i18next'
+import { CSSTransition } from 'react-transition-group'
 
-import { ESNotification } from 'sanar-ui/dist/Components/Organisms/MainMenu'
+import {
+    ESNotificationList,
+    ESNotificationItem
+} from 'sanar-ui/dist/Components/Organisms/MainMenu'
 import ESEvaIcon from 'sanar-ui/dist/Components/Atoms/EvaIcon'
 import ESButton from 'sanar-ui/dist/Components/Atoms/Button'
 import ESTypography from 'sanar-ui/dist/Components/Atoms/Typography'
@@ -10,8 +14,66 @@ import ESTabs, { ESTabPane } from 'sanar-ui/dist/Components/Atoms/Tabs'
 
 const intlPath = 'mainMenu.notifications.'
 
+const notificationsRead = [
+    {
+        icon: 'interaction',
+        action: 'Adré Cabral reagiu ao seu comentário',
+        text: `Ensure your most important links are available at the high level, and links that are a level deeper are relevant and impactful to more niche users`,
+        time: '2 horas atrás',
+        user: 'Adré Cabral'
+    },
+    {
+        action: 'Aula nova adicionada',
+        icon: 'new',
+        type: 'medium',
+        text: `A live sobre Saúde pública já vai começar.`,
+        time: '2 horas atrás'
+    },
+    {
+        action: 'Mapa mental atualizado',
+        icon: 'update',
+        type: 'high',
+        text: `Você está atrasado com suas atividades desta semana.`,
+        time: '2 horas atrás'
+    }
+]
+
+const notificationsUnread = [
+    {
+        read: true,
+        action: 'Desempenho positivo',
+        icon: 'performance',
+        text: `Ensure your most important links are available at the high level, and links that are a level deeper are relevant and impactful to more niche users`,
+        time: '2 horas atrás',
+        user: 'Adré Cabral'
+    },
+    {
+        read: true,
+        action: 'Live programada',
+        icon: 'live',
+        text: `A live sobre Saúde pública já vai começar.`,
+        time: '2 horas atrás'
+    },
+    {
+        read: true,
+        action: 'Atividade módulo 4',
+        icon: 'schedule',
+        text: `Você está atrasado com suas atividades desta semana.`,
+        time: '2 horas atrás'
+    },
+    {
+        read: true,
+        action: 'Termos legais atualizados',
+        icon: 'general',
+        text: `Você está atrasado com suas atividades desta semana.`,
+        time: '2 horas atrás'
+    }
+]
+
 const SANNotifications = ({ setTab }) => {
     const { t } = useTranslation()
+    const [reads, setReads] = useState(notificationsRead)
+    const [unreads, setUnreads] = useState(notificationsUnread)
 
     return (
         <>
@@ -56,40 +118,41 @@ const SANNotifications = ({ setTab }) => {
                             bold
                             color='white'
                             variant='text'
+                            onClick={() => setReads([])}
                         >
                             {t(`${intlPath}markAllRead`)}
                         </ESButton>
                     </div>
                     <div className='pl-md pr-md pb-md'>
-                        <ESNotification
-                            icon='interaction'
-                            action='Adré Cabral reagiu ao seu comentário'
-                            text={`Ensure your most important links are available at the high level, and links that are a level deeper are relevant and impactful to more niche users`}
-                            time='2 horas atrás'
-                            markAsRead={console.log}
-                            markAsUnread={console.log}
-                            user='Adré Cabral'
-                            labelMarkAsRead={t(`${intlPath}markRead`)}
-                            labelMarkAsUnead={t(`${intlPath}markUnread`)}
-                        />
-                        <ESNotification
-                            action='Aula nova adicionada'
-                            icon='new'
-                            type='medium'
-                            text={`A live sobre Saúde pública já vai começar.`}
-                            time='2 horas atrás'
-                            markAsRead={console.log}
-                            markAsUnread={console.log}
-                        />
-                        <ESNotification
-                            action='Mapa mental atualizado'
-                            icon='update'
-                            type='high'
-                            text={`Você está atrasado com suas atividades desta semana.`}
-                            time='2 horas atrás'
-                            markAsRead={console.log}
-                            markAsUnread={console.log}
-                        />
+                        <ESNotificationList>
+                            {reads.map((e, i) => (
+                                <CSSTransition
+                                    timeout={500}
+                                    classNames='read'
+                                    key={e.icon}
+                                >
+                                    <ESNotificationItem
+                                        {...e}
+                                        markAsRead={() => {
+                                            setUnreads([
+                                                ...unreads,
+                                                { ...e, read: true }
+                                            ])
+                                            setReads(
+                                                reads.filter((e, a) => i !== a)
+                                            )
+                                        }}
+                                        markAsUnread={console.log}
+                                        labelMarkAsRead={t(
+                                            `${intlPath}markRead`
+                                        )}
+                                        labelMarkAsUnread={t(
+                                            `${intlPath}markUnread`
+                                        )}
+                                    />
+                                </CSSTransition>
+                            ))}
+                        </ESNotificationList>
                     </div>
                 </ESTabPane>
                 <ESTabPane
@@ -118,48 +181,38 @@ const SANNotifications = ({ setTab }) => {
                             bold
                             color='white'
                             variant='text'
+                            onClick={() => setUnreads([])}
                         >
                             {t(`${intlPath}excludeRlreadyRead`)}
                         </ESButton>
                     </div>
                     <div className='pl-md pr-md pb-md'>
-                        <ESNotification
-                            read
-                            action='Desempenho positivo'
-                            icon='performance'
-                            text={`Ensure your most important links are available at the high level, and links that are a level deeper are relevant and impactful to more niche users`}
-                            time='2 horas atrás'
-                            markAsRead={console.log}
-                            markAsUnread={console.log}
-                            user='Adré Cabral'
-                        />
-                        <ESNotification
-                            read
-                            action='Live programada'
-                            icon='live'
-                            text={`A live sobre Saúde pública já vai começar.`}
-                            time='2 horas atrás'
-                            markAsRead={console.log}
-                            markAsUnread={console.log}
-                        />
-                        <ESNotification
-                            read
-                            action='Atividade módulo 4'
-                            icon='schedule'
-                            text={`Você está atrasado com suas atividades desta semana.`}
-                            time='2 horas atrás'
-                            markAsRead={console.log}
-                            markAsUnread={console.log}
-                        />
-                        <ESNotification
-                            read
-                            action='Termos legais atualizados'
-                            icon='general'
-                            text={`Você está atrasado com suas atividades desta semana.`}
-                            time='2 horas atrás'
-                            markAsRead={console.log}
-                            markAsUnread={console.log}
-                        />
+                        <ESNotificationList>
+                            {unreads.map((e, i) => (
+                                <CSSTransition
+                                    timeout={500}
+                                    classNames='unread'
+                                    key={e.icon}
+                                >
+                                    <ESNotificationItem
+                                        {...e}
+                                        markAsUnread={() =>
+                                            setUnreads(
+                                                unreads.filter(
+                                                    (e, a) => i !== a
+                                                )
+                                            )
+                                        }
+                                        labelMarkAsRead={t(
+                                            `${intlPath}markRead`
+                                        )}
+                                        labelMarkAsUnread={t(
+                                            `${intlPath}markUnread`
+                                        )}
+                                    />
+                                </CSSTransition>
+                            ))}
+                        </ESNotificationList>
                     </div>
                 </ESTabPane>
             </ESTabs>
