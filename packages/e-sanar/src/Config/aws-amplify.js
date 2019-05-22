646efad57f0886d3e00eed5e993d59134db7cb99
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-05-22 17:13:32 -0300

    feat(e-sanar): add aws-amplify configs

---
+++ packages/e-sanar/src/Config/aws-amplify.js
@@ -0,0 +1,9 @@
+const AWSAmplifyConfig = {
+    Auth: {
+        region: 'us-east-1',
+        userPoolId: 'us-east-1_yhtJbM70x',
+        userPoolWebClientId: '376aj1uj6n4utpvaqt4g9l7go9'
+    }
+}
+
+export default AWSAmplifyConfig
