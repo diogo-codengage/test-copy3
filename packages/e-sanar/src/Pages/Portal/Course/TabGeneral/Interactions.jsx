bf0fd97530dd9ead95a7012e4ca9509e0d990b2a
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-05-22 10:37:17 -0300

    feat(e-sanar): add alternative flows to interactions session

--- packages/e-sanar/src/Pages/Portal/Course/TabGeneral/Interactions.jsx
+++ packages/e-sanar/src/Pages/Portal/Course/TabGeneral/Interactions.jsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next'
 import ESListView from 'sanar-ui/dist/Components/Atoms/ListView'
 import ESQuestionListItem from 'sanar-ui/dist/Components/Molecules/QuestionListItem'
 import ESRecentSavedListItem from 'sanar-ui/dist/Components/Molecules/RecentSavedListItem'
+import ESMissContent from 'sanar-ui/dist/Components/Molecules/MissContent'
 import ESCard from 'sanar-ui/dist/Components/Molecules/Card'
 import ESSessionTitle from 'sanar-ui/dist/Components/Molecules/SessionTitle'
 import ESButton from 'sanar-ui/dist/Components/Atoms/Button'
@@ -17,6 +18,8 @@ import { differenceInDays } from 'date-fns'
 //assets
 import book from 'assets/images/book.svg'
 import video from 'assets/images/video.svg'
+import noQuestion from 'assets/images/empty-question.svg'
+import noBookmark from 'assets/images/empty-bookmark.svg'
 
 const SANInteractions = () => {
     const { t } = useTranslation()
@@ -49,6 +52,104 @@ const SANInteractions = () => {
         }
     }
 
+    const renderListComments = comments => {
+        if (comments.count >= 2) {
+            return comments.data.map(
+                (comment, index) =>
+                    index < 2 && (
+                        <ESQuestionListItem
+                            key={index}
+                            avatar={comment.user.profile_picture}
+                            title={comment.text}
+                            author={comment.user.name}
+                            responses={comment.answers}
+                            interactionTime={calcDiffDays(comment.created_at)}
+                            badgeInfo={t('global.you')}
+                            userIsAuthor={comment.user.id === idLoggedUser}
+                        />
+                    )
+            )
+        }
+
+        if (comments.count === 1) {
+            const comment = comments.data.pop()
+            return (
+                <>
+                    <ESQuestionListItem
+                        avatar={comment.user.profile_picture}
+                        title={comment.text}
+                        author={comment.user.name}
+                        responses={comment.answers}
+                        interactionTime={calcDiffDays(comment.created_at)}
+                        badgeInfo={t('global.you')}
+                        userIsAuthor={comment.user.id === idLoggedUser}
+                    />
+                    <ESMissContent
+                        text={t('courseDetails.recentCommentsOneComment')}
+                        buttonText={t(
+                            'courseDetails.recentCommentsOneCommentAction'
+                        )}
+                        image={noQuestion}
+                    />
+                </>
+            )
+        }
+
+        return (
+            <ESMissContent
+                text={t('courseDetails.recentCommentsOneComment')}
+                buttonText={t('courseDetails.recentCommentsOneCommentAction')}
+                image={noQuestion}
+                fullSpace
+            />
+        )
+    }
+
+    const renderListBookmarks = bookmarks => {
+        if (bookmarks.count >= 2) {
+            return bookmarks.data.map(
+                (bookmark, index) =>
+                    index < 2 && (
+                        <ESRecentSavedListItem
+                            key={index}
+                            avatar={bookmarkAvatar(bookmark.resource_type)}
+                            title={bookmark.resource.title}
+                            description={bookmark.resource.path}
+                        />
+                    )
+            )
+        }
+
+        if (bookmarks.count === 1) {
+            const bookmark = bookmarks.data.pop()
+
+            return (
+                <>
+                    <ESRecentSavedListItem
+                        avatar={bookmarkAvatar(bookmark.resource_type)}
+                        title={bookmark.resource.title}
+                        description={bookmark.resource.path}
+                    />
+                    <ESMissContent
+                        text={t('courseDetails.recentlySavedOneItem')}
+                        image={noBookmark}
+                    />
+                </>
+            )
+        }
+
+        return (
+            <>
+                <ESMissContent
+                    text={t('courseDetails.recentlySavedNoItems')}
+                    buttonText={t('courseDetails.recentlySavedNoItemsAction')}
+                    image={noBookmark}
+                    fullSpace
+                />
+            </>
+        )
+    }
+
     return (
         <SANPortalPagesContainer>
             <ESRow type='flex' className='interactions' gutter={24}>
@@ -78,29 +179,7 @@ const SANInteractions = () => {
                             </ESButton>
                         ]}
                     >
-                        <ESListView>
-                            {comments.data.map(
-                                (comment, index) =>
-                                    index < 2 && (
-                                        <ESQuestionListItem
-                                            key={index}
-                                            avatar={
-                                                comment.user.profile_picture
-                                            }
-                                            title={comment.text}
-                                            author={comment.user.name}
-                                            responses={comment.answers}
-                                            interactionTime={calcDiffDays(
-                                                comment.created_at
-                                            )}
-                                            badgeInfo={t('global.you')}
-                                            userIsAuthor={
-                                                comment.user.id === idLoggedUser
-                                            }
-                                        />
-                                    )
-                            )}
-                        </ESListView>
+                        <ESListView>{renderListComments(comments)}</ESListView>
                     </ESCard>
                 </ESCol>
                 <ESCol
@@ -130,19 +209,7 @@ const SANInteractions = () => {
                         ]}
                     >
                         <ESListView>
-                            {bookmarks.data.map(
-                                (bookmark, index) =>
-                                    index <= 1 && (
-                                        <ESRecentSavedListItem
-                                            key={index}
-                                            avatar={bookmarkAvatar(
-                                                bookmark.resource_type
-                                            )}
-                                            title={bookmark.resource.title}
-                                            description={bookmark.resource.path}
-                                        />
-                                    )
-                            )}
+                            {renderListBookmarks(bookmarks)}
                         </ESListView>
                     </ESCard>
                 </ESCol>
