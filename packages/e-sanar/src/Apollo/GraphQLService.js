a3327f952104edd117425fcd500773771c501e20
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-05-29 18:21:57 -0300

    refactor(e-sanar): change GraphQL API url

--- packages/e-sanar/src/Apollo/GraphQLService.js
+++ packages/e-sanar/src/Apollo/GraphQLService.js
@@ -3,8 +3,7 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 
 const client = new ApolloClient({
-    uri:
-        'http://elb-newesanar-bff-qa-f30dd4b4744e4ff6.elb.us-east-1.amazonaws.com:4000/graphql',
+    uri: 'http://167.99.151.251:4000/graphql',
     request: async operation =>
         operation.setContext({
             headers: {
