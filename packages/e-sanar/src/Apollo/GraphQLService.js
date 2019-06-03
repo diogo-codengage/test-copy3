bb9451efe60c90be3607e9e0df1498e03000d5d4
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-06-03 10:15:00 -0300

    refactor(e-sanar): change GraphQL URL API

--- packages/e-sanar/src/Apollo/GraphQLService.js
+++ packages/e-sanar/src/Apollo/GraphQLService.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import { Auth } from 'aws-amplify'
+
+const getCurrentTokenSession = () => {
+    return Auth.currentSession()
+        .then(response => {
+            // return `Bearer ${response.accessToken.jwtToken}`
+            return `Bearer iambatman`
+        })
+        .catch(console.log)
+}
 
 const client = new ApolloClient({
-    uri: 'http://167.99.151.251:4000/graphql',
+    uri: 'http://178.62.228.241:4000/graphql',
+    onError: console.log,
     request: async operation =>
         operation.setContext({
             headers: {
-                Authorization: 'Bearer iambatman'
+                Authorization: await getCurrentTokenSession()
             }
         })
 })
