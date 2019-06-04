ed7e5b4176c71aadffc344fba9ca5b55152b5308
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-06-04 18:11:53 -0300

    refactor(e-sanar): add environmnets to project

--- packages/e-sanar/src/Config/aws-amplify.js
+++ packages/e-sanar/src/Config/aws-amplify.js
@@ -1,17 +1,10 @@
 const AWSAmplifyConfig = {
     Auth: {
-        identityPoolId: 'us-east-1:ea2c0afe-53a5-41e2-85fe-27b558ed9ef4',
-        region: 'us-east-1',
-        userPoolId: 'us-east-1_yhtJbM70x',
-        userPoolWebClientId: '376aj1uj6n4utpvaqt4g9l7go9'
+        region: process.env.REACT_APP_AWS_COGNITO_REGION,
+        userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID,
+        userPoolWebClientId:
+            process.env.REACT_APP_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID
     }
 }
-// const AWSAmplifyConfig = {
-//     Auth: {
-//         region: 'us-east-1',
-//         userPoolId: 'us-east-1_OcvuY29jl',
-//         userPoolWebClientId: '5dklrtlhqhp8h55abr7rgo7ns8'
-//     }
-// }
 
 export default AWSAmplifyConfig
