0a26d8ef1ba62b3d5902130222a530c3196157d5
Adriano Moreira <adriano@codengage.com>
2019-06-24 17:07:33 -0300

    feat: questions rm

--- toGitHub.py
+++ toGitHub.py
@@ -9,27 +9,28 @@ def execComand(command):
     if exitCode != 0:
         print('=======================================')
         print('Ooops !!!')
-        print('exit code is {}'.format(exitCode))
+        print('command: {}'.format(command))
+        print('exit code: {}'.format(exitCode))
         exit(0)
 
 preCommands = [
-#    'rm -rv packages\\sanar-ui\\dist',
-#    'rm -rv packages\\sanar-rm-questions\\dist',
+    'rm -rv packages\\sanar-ui\\dist',
+    'rm -rv packages\\sanar-rm-questions\\dist',
 ]
 
 for command in preCommands:
     os.system(command)
 
 commands = [
-#    'git pull origin develop',
-#    'yarn sanarui:build',
-#    'yarn sanarrm:build',
-#    'git checkout develop',
-#    'git merge questions-rm',
-#    'git push',
-#    'git checkout master',
-#    'git merge develop',
-#    'git push github',
+    'git pull origin develop',
+    'yarn sanarui:build',
+    'yarn sanarrm:build',
+    'git checkout develop',
+    'git merge questions-rm',
+    'git push',
+    'git checkout master',
+    'git merge develop',
+    'git push github',
     'git checkout questions-rm'
 ]
 
