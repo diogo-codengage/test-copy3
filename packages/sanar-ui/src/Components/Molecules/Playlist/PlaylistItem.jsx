25d5a2190627005c82d36fd98d7aabedf48159c2
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-06-29 18:48:13 -0300

    refactor(sanar-ui): change time attr into playlist

--- packages/sanar-ui/src/Components/Molecules/Playlist/PlaylistItem.jsx
+++ packages/sanar-ui/src/Components/Molecules/Playlist/PlaylistItem.jsx
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types'
 import ESTypography from '../../Atoms/Typography'
 import ESEvaIcon from '../../Atoms/EvaIcon'
 
+import { esUtilConvertSecondsToTime } from 'sanar-ui/dist/Util/Date'
+
 const icons = {
     Video: 'play-circle-outline',
     Document: 'book-open-outline',
@@ -12,11 +14,14 @@ const icons = {
 }
 
 const ESPlaylist = ({ className, index, item, current, onClick }) => {
-    const { title, time, finish } = item[item.resource_type.toLowerCase()]
+    const { title, progress, durationInSeconds } = item[
+        item.resource_type.toLowerCase()
+    ]
     const classes = classNames(
         'es-playlist-list__item',
         {
-            'es-playlist-list__item--finish': finish,
+            'es-playlist-list__item--finish':
+                progress && progress.percentage === 100,
             'es-playlist-list__item--current': current
         },
         className
@@ -41,9 +46,9 @@ const ESPlaylist = ({ className, index, item, current, onClick }) => {
                     {title}
                 </ESTypography>
             </div>
-            {time && (
+            {durationInSeconds && (
                 <ESTypography variant='overline' className='time'>
-                    {time}
+                    {esUtilConvertSecondsToTime(durationInSeconds)}
                 </ESTypography>
             )}
         </div>
