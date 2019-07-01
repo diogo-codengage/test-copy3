9789024cf83dbf3b857d4709530f29a40c9d3c8d
Adriano Moreira <adriano@codengage.com>
2019-07-01 14:41:56 -0300

    feat(questions-rm): adiciona validacao no filtro

--- packages/sanar-rm-questions/src/Pages/Questions/Question/Question.tsx
+++ packages/sanar-rm-questions/src/Pages/Questions/Question/Question.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
 import { QuestionTemplate } from './QuestionTemplate'
 import { useQuestionsContext } from '../QuestionsContext'
-import { QuestionsInputFilter } from '../../../BFF/QuestionsInputFilter'
 import { BFFService } from '../../../BFF/BFFService'
-import { normalizeString } from '../../../Util/normalizeString'
-import { toCorrelacaoTagName } from '../../../Util/corelacaoEntrePlataformas'
 
 export const Question = () => {
 
@@ -13,88 +10,14 @@ export const Question = () => {
     const [stats, setStats] = useState()
     const [loading, setLoading] = useState(false)
 
-    const getParams = async ():Promise<QuestionsInputFilter> => {
-        if(!!questionsCtx.course) {
-            return getParamsFromCourse()
-        }
-        return getParamsFromFilters()
-    }
-
-    const normalizeEndCompare = (o1: string, o2: string) =>{
-        return  normalizeString(o1) === normalizeString(o2)
-    }
-
-    const getParamsFromCourse = async ():Promise<QuestionsInputFilter> => {
-
-        const tagsIds = questionsCtx.allTags
-            .filter(t => normalizeEndCompare(t.label, toCorrelacaoTagName(questionsCtx.course.moduleName)))
-            .map(t => t.value)
-
-        const specialtiesIds = questionsCtx.allSpecialties.flatMap(s => s.children)
-            .filter(v => normalizeEndCompare(v.label,questionsCtx.course.subSpecialtyName))
-            .map(v => v.value)
-
-        return {
-            specialtiesIds,
-            institutionsIds: [],
-            tagsIds,
-            states: [],
-            years: [],
-            isCommentedByExpert: null,
-        }
-    }
-
-    const getParamsFromFilters = (): QuestionsInputFilter  => {
-
-        const idsSelectedSubSpecialties = questionsCtx.selectedSubSpecialties.map(ss => ss.value);
-
-        return  {
-            specialtiesIds: questionsCtx.selectedSpecialties
-                .filter( s => !s.children.map(ss => ss.value).find(ss => idsSelectedSubSpecialties.includes(ss) ) )
-                .map(s => s.value)
-                .concat(questionsCtx.selectedSubSpecialties
-                    .map(s => s.value)),
-            institutionsIds: questionsCtx.selectedInstitutions.map( i => i.value ),
-            tagsIds: questionsCtx.selectedTags.map( t => t.value),
-            states: questionsCtx.selectedStates.map(s => s.value),
-            years: questionsCtx.selectedYears.map(v => v.value),
-            isCommentedByExpert: questionsCtx.isCommentedByExpert,
-        }
-    }
-
-    const pushQuestions = (moreQuestions) => {
-
-        if(questionsCtx.questions.length > 1){
-            return;
-        }
-        console.log('pushQuestions')
-        let questions = questionsCtx.questions
-
-        questions.push(...moreQuestions)
-        if (!questionsCtx.currentQuestion) {
-            questionsCtx.setCurrentQuestion(questions.shift())
-        }
-        questionsCtx.setQuestions(questions)
-    }
-
     const loadNextQuestion = () => {
         let questions = questionsCtx.questions
         questionsCtx.setCurrentQuestion(questions.shift())
         questionsCtx.setQuestions(questions)
         if (questions.length === 1) {
-            loadMoreQuestions()
+            questionsCtx.loadMoreQuestions(false)
         }
         questionsCtx.setCurrentAnswerId(null)
-    }
-
-    const loadMoreQuestions = async () => {
-            const filters = await getParams()
-            BFFService.loadMoreQuestions(filters)
-                .then(function({ data }) {
-                    questionsCtx.setQuestionsRequests(questionsCtx.questionsRequests + 1)
-                    return pushQuestions(data.questions.data)
-                })
-
     }
 
     const onSkipQuestion = ({id}) => {
@@ -108,7 +31,7 @@ export const Question = () => {
     }
 
     if(!questionsCtx.currentQuestion) {
-        loadMoreQuestions()
+        questionsCtx.loadMoreQuestions(false);
     }
 
     const onConfirmResponse = (alternativeId) => {
@@ -128,6 +51,10 @@ export const Question = () => {
             }
             questionsCtx.setCurrentAnswerId(answerId)
         })
+    }
+
+    if (!questionsCtx.currentQuestion && questionsCtx.questions.length > 0 ) {
+        loadNextQuestion();
     }
 
     return (
