5568f2c75ea5ab2fb767be1e720adf20a1ed221b
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-07-09 11:01:27 -0300

    chore(core): change codeship.sh

--- codeship.sh
+++ codeship.sh
@@ -2,11 +2,25 @@
 
 if [ "$CI_BRANCH" = "master" ];
 then
-NODE_ENV='production'
-echo 'production environment'
+  NODE_ENV='production'
+  echo 'production environment'
+elif [ "$CI_BRANCH" = "staging" ]
+then
+  NODE_ENV='staging'
+  echo 'production environment'
 else
-NODE_ENV='development'
-echo 'development environment'
+  NODE_ENV='development'
+  yarn esanar:build:development
 fi
 
 export NODE_ENV
+
+# if [ $CI_BRANCH = "master" ];
+# then
+# yarn esanar:build:production
+# elif [$CI_BRANCH = "staging"]
+# then
+# yarn esanar:build:staging
+# else
+# yarn esanar:build:development
+# fi
