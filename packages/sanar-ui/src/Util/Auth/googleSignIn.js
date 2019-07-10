fac4222c0b9c69693eb24b30d59e4019a6180710
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-07-10 09:45:11 -0300

    refactor(sanar-ui | e-sanar): add .env keys to social logins

--- packages/sanar-ui/src/Util/Auth/googleSignIn.js
+++ packages/sanar-ui/src/Util/Auth/googleSignIn.js
@@ -1,11 +1,9 @@
 import { Auth } from 'aws-amplify'
 
-// To federated sign in from Google
-
 const esGoogleSignIn = () => {
     return Auth.federatedSignIn({ provider: 'Google' }).then(
         googleUser => {
-            console.log(googleUser);
+            console.log(googleUser)
             return getAWSCredentials(googleUser)
         },
         error => {
@@ -14,40 +12,29 @@ const esGoogleSignIn = () => {
     )
 }
 
-export const esGoogleCreateScript = () => {
+export const esGoogleCreateScript = key => {
     // load the Google SDK
     const script = document.createElement('script')
     script.src = 'https://apis.google.com/js/platform.js'
     script.async = true
-    script.onload = initGapi
+    script.onload = () => initGapi(key)
     document.body.appendChild(script)
 }
 
-const initGapi = () => {
+const initGapi = key => {
     // init the Google SDK client
     const g = window.gapi
     g.load('auth2', function() {
         g.auth2.init({
-            client_id:
-                '807324955316-01gamhmmlffq8oc15r2huvnrahigdi5v.apps.googleusercontent.com',
-            // authorized scopes
+            client_id: key,
             scope: 'profile email openid'
         })
     })
 }
 
 const getAWSCredentials = async googleUser => {
-    console.log('Starting signIn with Google Button...');
-    /*
-    const { id_token, expires_at } = googleUser.getAuthResponse()
-    const profile = googleUser.getBasicProfile()
-    let user = {
-        email: profile.getEmail(),
-        name: profile.getName()
-    }
+    console.log('Starting signIn with Google Button...')
 
-    await Auth.federatedSignIn('google', { token: id_token, expires_at }, user)
-    */
     await Auth.federatedSignIn({ provider: 'Google' })
 }
 
