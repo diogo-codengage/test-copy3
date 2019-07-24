b9159042013c825cc9e6e74b88fa0e12ef73d92d
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-07-24 16:21:29 -0300

    feat(e-sanar): add lazy loading for all routes

--- packages/e-sanar/src/Pages/Portal/index.jsx
+++ packages/e-sanar/src/Pages/Portal/index.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Suspense } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { Query } from 'react-apollo'
-
-import ESSplashLoader from 'sanar-ui/dist/Components/Atoms/SplashLoader'
+import SANSplashLoader from 'Components/SplashLoader'
 
 import { GET_ME } from 'Apollo/Me/query'
 import { useAuthContext } from 'Hooks/auth'
 import { useApolloContext } from 'Hooks/apollo'
 
 import SANPortalLayout from './Layout'
-import SANCoursePage from './Course'
-import SANQuestionsPage from './Questions'
-import SANClassroomPage from './Classrom'
-import SANMyAccountChangePassword from './MyAccount'
-import SANHelpCenter from './HelpCenter'
 import { SANPortalProvider } from './Context'
 import ESDefaultError from '../Portal/Errors/Default'
+
+const SANCoursePage = React.lazy(() => import('./Course'))
+const SANQuestionsPage = React.lazy(() => import('./Questions'))
+const SANMyAccountChangePassword = React.lazy(() => import('./MyAccount'))
+const SANHelpCenter = React.lazy(() => import('./HelpCenter'))
+const SANClassroomPage = React.lazy(() => import('./Classrom'))
 
 const SANPortalRoutes = ({ match: { url } }) => {
     const client = useApolloContext()
@@ -33,41 +33,49 @@ const SANPortalRoutes = ({ match: { url } }) => {
             {({ loading, error }) => (
                 <>
                     {loading && !error ? (
-                        <ESSplashLoader />
+                        <SANSplashLoader />
                     ) : !loading && error ? (
                         <ESDefaultError />
                     ) : (
                         <SANPortalProvider>
                             <SANPortalLayout>
-                                <Switch>
-                                    <Route
-                                        path={`${url}/curso`}
-                                        strict
-                                        component={SANCoursePage}
-                                    />
-                                    <Route
-                                        path={`${url}/banco-questoes`}
-                                        component={SANQuestionsPage}
-                                    />
-                                    <Route
-                                        path={`${url}/minha-conta`}
-                                        component={SANMyAccountChangePassword}
-                                    />
-                                    <Route
-                                        path={`${url}/central-ajuda`}
-                                        component={SANHelpCenter}
-                                    />
-                                    <Route
-                                        path={`${url}/sala-aula/:moduleId/:type?/:resourceId?`}
-                                        component={SANClassroomPage}
-                                    />
-                                    <Route
-                                        path={[`${url}/`, `${url}`]}
-                                        render={() => (
-                                            <Redirect to={`${url}/curso`} />
-                                        )}
-                                    />
-                                </Switch>
+                                <Suspense
+                                    fallback={
+                                        <SANSplashLoader size='flexible' />
+                                    }
+                                >
+                                    <Switch>
+                                        <Route
+                                            path={`${url}/curso`}
+                                            strict
+                                            component={SANCoursePage}
+                                        />
+                                        <Route
+                                            path={`${url}/banco-questoes`}
+                                            component={SANQuestionsPage}
+                                        />
+                                        <Route
+                                            path={`${url}/minha-conta`}
+                                            component={
+                                                SANMyAccountChangePassword
+                                            }
+                                        />
+                                        <Route
+                                            path={`${url}/central-ajuda`}
+                                            component={SANHelpCenter}
+                                        />
+                                        <Route
+                                            path={`${url}/sala-aula/:moduleId/:type?/:resourceId?`}
+                                            component={SANClassroomPage}
+                                        />
+                                        <Route
+                                            path={[`${url}/`, `${url}`]}
+                                            render={() => (
+                                                <Redirect to={`${url}/curso`} />
+                                            )}
+                                        />
+                                    </Switch>
+                                </Suspense>
                             </SANPortalLayout>
                         </SANPortalProvider>
                     )}
