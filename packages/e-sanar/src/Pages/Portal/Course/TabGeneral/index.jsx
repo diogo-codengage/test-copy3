7c441d6757707b4fbf80b5d02d971c330bfa25be
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-07-26 17:46:06 -0300

    refactor(e-sanar): remove bookmarks from general tab

--- packages/e-sanar/src/Pages/Portal/Course/TabGeneral/index.jsx
+++ packages/e-sanar/src/Pages/Portal/Course/TabGeneral/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
 import SANPerformance from './Performance'
-import SANInteractions from './Interactions'
 import SANProfessors from './Professors'
 
 const SANCourseTabGeneral = () => (
     <div className='san-tab-course-general'>
         <SANPerformance />
-        <SANInteractions />
         <SANProfessors />
     </div>
 )
