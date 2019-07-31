0a3f25a208388500b47130e453219c4abe913179
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-07-31 16:26:09 -0300

    feat(e-sanar): add Input on Bookmarks SessionTitle

--- packages/e-sanar/src/Pages/Portal/Bookmark/List/Header.jsx
+++ packages/e-sanar/src/Pages/Portal/Bookmark/List/Header.jsx
@@ -3,8 +3,8 @@ import { useTranslation } from 'react-i18next'
 import ESTypography from 'sanar-ui/dist/Components/Atoms/Typography'
 import ESSessionTitle from 'sanar-ui/dist/Components/Molecules/SessionTitle'
 import SANPortalPagesContainer from 'Pages/Portal/Layout/Container'
-// import ESInput from 'sanar-ui/dist/Components/Atoms/Input'
-// import ESEvaIcon from 'sanar-ui/dist/Components/Atoms/EvaIcon'
+import ESInput from 'sanar-ui/dist/Components/Atoms/Input'
+import ESEvaIcon from 'sanar-ui/dist/Components/Atoms/EvaIcon'
 
 const intlPath = 'bookmark.'
 
@@ -16,21 +16,25 @@ const SANBookmarksHeader = () => {
             <SANPortalPagesContainer>
                 <ESSessionTitle
                     title={
-                        <ESTypography level={4}>
+                        <ESTypography className='header__title' level={4}>
                             {t(`${intlPath}title`)}
                         </ESTypography>
                     }
                     subtitle={
-                        <ESTypography variant='subtitle2'>
+                        <ESTypography
+                            className='header__subtitle'
+                            ellipsis
+                            variant='subtitle2'
+                        >
                             {t(`${intlPath}subtitle`)}
                         </ESTypography>
                     }
-                    // extra={
-                    //     <ESInput
-                    //         prefix={<ESEvaIcon name='search-outline' />}
-                    //         placeholder={t(`${intlPath}search`)}
-                    //     />
-                    // }
+                    extra={
+                        <ESInput
+                            prefix={<ESEvaIcon name='search-outline' />}
+                            placeholder={t(`${intlPath}search`)}
+                        />
+                    }
                 />
             </SANPortalPagesContainer>
         </div>
