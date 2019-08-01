3462960fea1fc2dd83f4b9f61d7271c899ee28bd
Daniel-Silva <daniel_f.s_@hotmail.com>
2019-08-01 17:36:52 -0300

    refactor(sana-ui): migrate Button and Icon as ts

--- packages/sanar-ui/src/Components/Atoms/Icon/Icon.tsx
+++ packages/sanar-ui/src/Components/Atoms/Icon/Icon.tsx
@@ -3,20 +3,11 @@ import PropTypes from 'prop-types'
 import Icon from 'antd/lib/icon'
 import classNames from 'classnames'
 
-const ESIcon = ({ className, fontSize, style, color, ...props }) => {
-    const classes = classNames('es-icon', className, {
-        [`text-${color}`]: color
-    })
-
-    return (
-        <Icon className={classes} {...props} style={{ ...style, fontSize }} />
-    )
-}
-
-ESIcon.propTypes = Object.assign(
+const propTypes = Object.assign(
     { ...Icon['propTypes'] },
     {
         className: PropTypes.string,
+        color: PropTypes.string,
         style: PropTypes.any,
         fontSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
         theme: PropTypes.oneOf(['filled', 'outlined', 'twoTone']),
@@ -27,6 +18,24 @@ ESIcon.propTypes = Object.assign(
     }
 )
 
-ESIcon.defaultProps = Icon['defaultProps']
+type IPros = PropTypes.InferProps<typeof propTypes>
+
+const ESIcon: React.FC<IPros> = ({
+    className,
+    fontSize,
+    style,
+    color,
+    ...props
+}) => {
+    const classes = classNames('es-icon', className, {
+        [`text-${color}`]: color
+    })
+    return (
+        <Icon className={classes} {...props} style={{ ...style, fontSize }} />
+    )
+}
+
+ESIcon.propTypes = propTypes
+// ESIcon.defaultProps = Icon['defaultProps']
 
 export default ESIcon
