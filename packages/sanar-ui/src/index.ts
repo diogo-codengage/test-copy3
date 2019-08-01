c1b6df0d4d3295288fea8ca7421d5ec2e74aeef1
Adriano Moreira <adriano@codengage.com>
2019-08-01 15:46:40 -0300

    refactor(sanar-ui): start migration to typscript

---
+++ packages/sanar-ui/src/index.ts
@@ -0,0 +1,6 @@
+import ESBadge  from './Components/Atoms/Badge'
+import _ESButton  from './Components/Atoms/Button'
+import ESIcon from './Components/Atoms/Icon'
+
+
+export const ESButton = _ESButton
