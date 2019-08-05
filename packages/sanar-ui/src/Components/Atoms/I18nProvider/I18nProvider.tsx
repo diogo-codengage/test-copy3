ee979a65160b07f0cff4743ea234baf2b20aa69f
Daniel-Silva <daniel_f.s_@hotmail.com>
2019-08-05 16:17:14 -0300

    refactor(sanar-ui): migrate I18nProvider to ts

--- packages/sanar-ui/src/Components/Atoms/I18nProvider/I18nProvider.tsx
+++ packages/sanar-ui/src/Components/Atoms/I18nProvider/I18nProvider.tsx
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types'
 
 import { I18nextProvider } from 'react-i18next'
 
-// import i18n from '../../../Config/i18n'
+type IProps = PropTypes.InferProps<typeof propTypes>
 
-const ESI18nProvider = ({ children, i18n }) => (
+const ESI18nProvider: React.FC<IProps>=({ children, i18n }) => (
     <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
 )
 
-ESI18nProvider.propTypes = {
+const propTypes = Object.assign({
     children: PropTypes.node,
     i18n: PropTypes.object
-}
-ESI18nProvider.defaultProps = {
-    // i18n
-}
+})
+
+ESI18nProvider.propTypes = propTypes
+
+ESI18nProvider.defaultProps = {}
 
 export default ESI18nProvider
