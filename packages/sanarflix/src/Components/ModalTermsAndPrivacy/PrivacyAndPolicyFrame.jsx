703c1216a913b363d5ec63a3e84b0b8b332ac9c3
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-08-09 18:04:01 -0300

    refactor(sanarflix): change spin validation to show

--- packages/sanarflix/src/Components/ModalTermsAndPrivacy/PrivacyAndPolicyFrame.jsx
+++ packages/sanarflix/src/Components/ModalTermsAndPrivacy/PrivacyAndPolicyFrame.jsx
@@ -9,7 +9,7 @@ const FLXPrivacyAndPolicyFrame = () => {
     }
     return (
         <>
-            <ESSpin flex loading={loading} minHeight='100%' />
+            <ESSpin flex spinning={loading} minHeight='100%' />
             <iframe
                 title='privacy-and-policy-frame'
                 onLoad={manipuleLoading}
