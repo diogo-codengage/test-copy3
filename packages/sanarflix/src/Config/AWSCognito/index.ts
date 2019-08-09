ca045b8b8bc3908553082bc8f65304cf7383f3b5
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-08-09 14:51:01 -0300

    refactor(sanarflix): change cognito config file

--- packages/sanarflix/src/Config/AWSCognito/index.ts
+++ packages/sanarflix/src/Config/AWSCognito/index.ts
@@ -1,11 +1,47 @@
-type FLXCognitoConfigTypes = {
-    UserPoolId: string
-    ClientId: string
-}
+import {
+    CognitoUserPool,
+    CognitoUser,
+    ICognitoUserData,
+    ICognitoUserPoolData
+} from 'amazon-cognito-identity-js'
 
-const FLXAWSCognitoConfig: FLXCognitoConfigTypes = {
+const FLXAWSCognitoConfigKeys: ICognitoUserPoolData = {
     UserPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID || '',
     ClientId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_WEB_CLIENT_ID || ''
 }
 
-export default FLXAWSCognitoConfig
+let user: CognitoUser
+let userEmail: string
+let userData: ICognitoUserData
+const userPool: CognitoUserPool = new CognitoUserPool(FLXAWSCognitoConfigKeys)
+
+const constructor = email => {
+    userEmail = email
+
+    userData = {
+        Username: userEmail,
+        Pool: userPool
+    }
+
+    user = new CognitoUser(userData)
+    return { user }
+}
+
+const getStorageEmail = (): string => {
+    const emailKey =
+        Object.keys(localStorage).find(
+            item => item.indexOf('LastAuthUser') > -1
+        ) || ''
+    return localStorage.getItem(emailKey) || ''
+}
+
+const getInstance = (email?: string | null) => {
+    const newEmail = email || getStorageEmail()
+    if (!user || email !== userEmail) {
+        return constructor(newEmail)
+    }
+
+    return { user }
+}
+
+export { getInstance }
