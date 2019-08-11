2964a1929b1e67320ff596f23994e016712ace5a
Adriano Moreira <adriano@codengage.com>
2019-08-11 18:41:55 -0300

    refactor(sanar-ui): migrate imput ts

--- packages/sanar-ui/src/index.ts
+++ packages/sanar-ui/src/index.ts
@@ -1,6 +1,11 @@
+import ESAlternative  from './Components/Atoms/Alternative'
 import ESBadge  from './Components/Atoms/Badge'
-import _ESButton  from './Components/Atoms/Button'
+import ESButton  from './Components/Atoms/Button'
 import ESIcon from './Components/Atoms/Icon'
 
-
-export const ESButton = _ESButton
+export {
+    ESAlternative,
+    ESBadge,
+    ESButton,
+    ESIcon
+}
