05f33695d9d694bb92469966650ed9958a121a5f
Adriano Moreira <adriano@codengage.com>
2019-08-11 18:36:52 -0300

    refactor(sanar-ui): migrate to ts, add sanar-ui-demo

---
+++ packages/sanar-ui-demo/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.less'
+import 'sanar-ui/dist/Config/i18n'
+
+import {ESButton} from 'sanar-ui/dist'
+
+const App: React.FC = () => {
+
+    return (
+	    <div>
+		    <ESButton  >foo</ESButton>
+	    </div>
+    )
+}
+
+export default App
+
