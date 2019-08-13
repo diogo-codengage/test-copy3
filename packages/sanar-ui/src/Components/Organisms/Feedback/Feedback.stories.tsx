f49aedcc6fb6e731463883dc442059b39b0dcf53
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-08-13 15:29:43 -0300

    refactor(sanar-ui): move from packages to libs

--- packages/sanar-ui/src/Components/Organisms/Feedback/Feedback.stories.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import ESFeedback from './Feedback'
-
-storiesOf('Organisms.Feedback', module).add('Simple', () => (
-    <ESFeedback />
-))
