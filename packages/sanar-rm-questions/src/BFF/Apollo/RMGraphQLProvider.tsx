0d5ef00ee7d517f1293575e974834747f6ed43fd
Diego Costa <diego@diegocosta.com.br>
2019-08-19 11:17:14 -0300

    fix(rm-176): fixed where format in questions query

--- packages/sanar-rm-questions/src/BFF/Apollo/RMGraphQLProvider.tsx
+++ packages/sanar-rm-questions/src/BFF/Apollo/RMGraphQLProvider.tsx
@@ -5,14 +5,14 @@ import { isDevEnvironment, isLocalhost, isProdEnvironment } from '../../Util/env
 import { getUserToken } from '../../Util/getUserToken'
 
 const getGraphqlUri = () => {
-    const local = 'http://nlb-residencia-qa-aa2d4c0241e3f160.elb.us-east-1.amazonaws.com/graphql';
+    const dev = 'http://nlb-residencia-qa-aa2d4c0241e3f160.elb.us-east-1.amazonaws.com/graphql';
     const prod = 'https://bff.sanarresidenciamedica.com.br/graphql'
 
     switch(true) {
         case isLocalhost():
-            return local // TODO: fix this, change to local
+            return dev // TODO: fix this, change to local
         case isDevEnvironment():
-            return prod // TODO: fix this, change to dev
+            return dev // TODO: fix this, change to dev
         case isProdEnvironment():
             return prod
         default:
