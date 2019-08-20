785d9ce4385951242afeaa84316816611b15d649
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-08-20 11:04:40 -0300

    refactor(sanarflix): remove useMemo dependency from SignIn

--- packages/sanarflix/src/Pages/Auth/SignIn/index.tsx
+++ packages/sanarflix/src/Pages/Auth/SignIn/index.tsx
@@ -58,6 +58,7 @@ const FLXSignIn: React.FC<any> = ({ history }) => {
                 </ESCol>
             </ESRow>
         ),
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         []
     )
 
