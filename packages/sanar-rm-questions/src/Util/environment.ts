dafda8e2ba3415341acd6d11a6cfad822759f136
Diego Costa <diego@diegocosta.com.br>
2019-08-20 13:32:48 -0300

    removed NODE_ENV

--- packages/sanar-rm-questions/src/Util/environment.ts
+++ packages/sanar-rm-questions/src/Util/environment.ts
@@ -1,11 +1,9 @@
-const { NODE_ENV } = process.env;
 
 export const isLocalhost = () => {
     return window.location.href.startsWith('http://localhost')
 }
 
 export const isDevEnvironment = () => {
-    // return NODE_ENV === 'development'
     return window.location.href.indexOf('plataforma-cursos-residencia-dev') > -1;
 }
 
