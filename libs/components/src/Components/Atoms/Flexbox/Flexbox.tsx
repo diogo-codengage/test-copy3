1c94712857ba984b2ccbb476834c8eca6c5fcb93
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-08-21 15:18:43 -0300

    refactor(@sanar/components): change SANFlexbox style

--- libs/components/src/Components/Atoms/Flexbox/Flexbox.tsx
+++ libs/components/src/Components/Atoms/Flexbox/Flexbox.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
-import { flexbox, FlexboxProps, ColorProps, compose } from 'styled-system'
+import {
+    flexbox,
+    FlexboxProps,
+    SpaceProps,
+    ColorProps,
+    compose
+} from 'styled-system'
 
 import { SANStyled } from 'Theme/createTheme'
 
-interface IProps extends FlexboxProps, ColorProps {
-    displayFlex?: boolean
-}
+interface IProps extends FlexboxProps, ColorProps {}
 
 const SANFlexbox: React.FC<IProps> = SANStyled('div')`
     ${compose(flexbox)}
+    display: flex;
 `
 
 export default SANFlexbox
