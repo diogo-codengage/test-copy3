e8e4bf76c560d5134aee716da507f2969630e33b
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-08-27 16:31:11 -0300

    feat(@sanar/components | sanarui): add onHome to menu

--- libs/components/src/Components/Organisms/MainMenu/MainMenu.tsx
+++ libs/components/src/Components/Organisms/MainMenu/MainMenu.tsx
@@ -2,15 +2,14 @@ import React, { useRef, forwardRef } from 'react'
 
 import ESMainMenu from 'sanar-ui/dist/Components/Organisms/MainMenu'
 
-type IProps = {
+interface IProps {
     onOpenOrClose: () => void
+    onHome: () => void
     showContinueBar?: boolean
 }
 
 const SANMainMenu: React.FC<IProps> = forwardRef(({ ...props }, ref) => {
     return <ESMainMenu ref={ref} {...props} />
 })
-
-SANMainMenu.defaultProps = {}
 
 export default SANMainMenu
