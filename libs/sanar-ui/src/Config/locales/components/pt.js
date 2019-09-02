d3819a1e7fd15ec141d636ec7aa5816e65ebfe3a
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-09-02 13:53:54 -0300

    feat(@components): add ClassroomMenuHeader translation

--- libs/sanar-ui/src/Config/locales/components/pt.js
+++ libs/sanar-ui/src/Config/locales/components/pt.js
@@ -1,7 +1,8 @@
 export default {
     global: {
         theme: 'Tema',
-        courseProgress: 'Progresso do Curso'
+        courseProgress: 'Progresso do Curso',
+        goToBegin: 'Voltar para o início'
     },
     empty: {
         title: 'Não encontramos o que você buscou.'
