2df3790bbc2e3fbb132b13c8552b358e3a37c3ec
Felipe Oliveira <felipe@codengage.com>
2019-09-09 14:14:07 -0300

    refactor(sanarui): change "Documento" translation

--- libs/sanar-ui/src/Config/locales/sanarflix/pt.js
+++ libs/sanar-ui/src/Config/locales/sanarflix/pt.js
@@ -15,7 +15,7 @@ export default {
             incomplete: 'Incompletos'
         },
         resourceTypes: {
-            document: 'Document',
+            document: 'Documento',
             video: 'Video',
             question: 'Questão'
         },
