4735f548f5ad267f5545716d2fab900a4c4cf526
Vinicius Lohmann Kieling <vinicius@codengage.com>
2019-09-12 17:10:34 -0300

    feat(@components): add resouce types primary icons

---
+++ libs/components/src/Assets/index.js
@@ -0,0 +1,15 @@
+import typeIconPrimaryBook from './images/type-icons/primary/book.svg'
+import typeIconPrimaryFile from './images/type-icons/primary/file.svg'
+import typeIconPrimaryFlowchart from './images/type-icons/primary/flowchart.svg'
+import typeIconPrimaryMentalchart from './images/type-icons/primary/mentalchart.svg'
+import typeIconPrimaryQuestion from './images/type-icons/primary/question.svg'
+import typeIconPrimaryVideo from './images/type-icons/primary/video.svg'
+
+export {
+    typeIconPrimaryBook,
+    typeIconPrimaryFile,
+    typeIconPrimaryFlowchart,
+    typeIconPrimaryMentalchart,
+    typeIconPrimaryQuestion,
+    typeIconPrimaryVideo
+}
