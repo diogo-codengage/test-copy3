3824e3b7723736a1dc070c112ada8cad59e5cf85
Diogo Biz <diogo@codengage.com>
2019-10-17 10:25:08 -0300

    feat(components): define payload result search

--- libs/components/src/Components/Organisms/SearchResult/SearchResult.stories.tsx
+++ libs/components/src/Components/Organisms/SearchResult/SearchResult.stories.tsx
@@ -2,6 +2,69 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import SANSearchResult from './SearchResult'
 
+type ResourceType =
+    | 'Book'
+    | 'Course'
+    | 'Content'
+    | 'Question'
+    | 'Quiz'
+    | 'Video'
+    | 'Document'
+    | 'Download'
+
+interface Course {
+    id: string
+    name: string
+}
+
+interface Theme {
+    id: string
+    name: string
+}
+
+interface CoverPicture {
+    small: SizeImage
+    medium?: SizeImage
+    large?: SizeImage
+    original?: SizeImage
+}
+
+interface SizeImage {
+    filename: string
+    url: string
+}
+
+interface IExample {
+    resourceTitle: string
+    resourceId: string
+    resourceType: ResourceType
+    totalPages?: number
+    timeInSeconds?: number
+    coverPictures: CoverPicture
+    course?: Course
+    theme?: Theme
+    type?: string
+    isNew?: boolean
+    professorName?: string
+}
+
+const data: IExample[] = [
+    {
+        resourceTitle: 'Nome do Arquivo - Subtítulo do arquivo',
+        resourceId: '1',
+        resourceType: 'Course',
+        coverPictures: {
+            small: {
+                filename: '',
+                url: ''
+            }
+        },
+        type: 'string'
+    }
+]
+
+const renderItem = (item: IExample) => <div>{item.resourceTitle}</div>
+
 storiesOf('Organisms.SearchResult', module).add('Simple', () => (
-    <SANSearchResult />
+    <SANSearchResult<IExample> dataSource={data} renderItem={renderItem} />
 ))
--- libs/components/src/Components/Organisms/SearchResult/SearchResult.tsx
+++ libs/components/src/Components/Organisms/SearchResult/SearchResult.tsx
@@ -1,23 +1,16 @@
 import React, { useState } from 'react'
 
-import { useTranslation } from 'react-i18next'
-
 import { SANBox } from '../../Atoms/Box'
 
 export interface ISANSearchResultProps<T> {
     dataSource: T[]
-    renderItem: (item: T, index: number) => React.ReactNode
+    renderItem: (item: T, index: number, arr: T[]) => React.ReactNode
 }
 
 const SANSearchResultList = <T extends {}>(props: ISANSearchResultProps<T>) => {
     const { dataSource, renderItem } = props
-    const { t } = useTranslation('components')
 
-    return (
-        <SANBox>
-            {dataSource.map(renderItem)}
-        </SANBox>
-    )
+    return <SANBox>{dataSource.map(renderItem)}</SANBox>
 }
 
 export default SANSearchResultList
