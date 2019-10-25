94663cb09186fa02a60e7504861e4bbc4443714a
Diogo Biz <diogo@codengage.com>
2019-10-25 09:59:45 -0300

    feat(resmed): config apollo client

--- packages/resmed/src/index.tsx
+++ packages/resmed/src/index.tsx
@@ -5,16 +5,19 @@ import * as serviceWorker from './serviceWorker'
 
 import 'sanar-ui/dist/Config/i18n'
 
+import { RMGraphQLProvider } from './Apollo/GraphQLService'
 import { RMThemeProvider } from './Components/Theme'
 import { RMGlobalStyle } from './Styles'
 
 const RMApp: React.FC = () => (
-    <RMThemeProvider>
-        <>
-            <RMGlobalStyle />
-            <App />
-        </>
-    </RMThemeProvider>
+    <RMGraphQLProvider>
+        <RMThemeProvider>
+            <>
+                <RMGlobalStyle />
+                <App />
+            </>
+        </RMThemeProvider>
+    </RMGraphQLProvider>
 )
 
 ReactDOM.render(<RMApp />, document.getElementById('root'))
