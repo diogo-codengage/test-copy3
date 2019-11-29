6305861d3e092ef7190914d2d0b7fb04e3138b45
MarlosAugusto <marlos.gomes@codengage.com>
2019-11-29 11:01:38 -0200

    feat(resmed): segment track to event Session started

--- packages/resmed/src/Pages/Auth/index.tsx
+++ packages/resmed/src/Pages/Auth/index.tsx
@@ -6,6 +6,8 @@ import RMSplashLoader from 'Components/SplashLoader'
 
 import { getCognitoUser } from 'Config/AWSCognito'
 
+import { segmentTrack } from 'Config/Segment/track'
+
 const RMLogin = React.lazy(() => import('./Login'))
 const RMPasswordRecovery = React.lazy(() => import('./PasswordRecovery'))
 const RMNewPassword = React.lazy(() => import('./NewPassword'))
@@ -17,12 +19,25 @@ const RMAuth: React.FC<RouteComponentProps> = ({ match: { url } }) => {
         loading: true
     })
 
+    const handleTrack = async () => {
+        // User ID, Plataform ID, Date, Time, Device
+        // const datetime = new Date()
+        const data = {
+            'Plataform ID': process.env.REACT_APP_PLATFORM_ID
+            // Date: `${datetime.getDate()}/${datetime.getMonth() + 1}/${datetime.getFullYear()}`,
+            // Time: `${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}`,
+            // Device: null
+        }
+        segmentTrack(client, 'Session started', data)
+    }
+
     useEffect(() => {
         const cognitoUser = getCognitoUser()
 
         if (!!cognitoUser) {
             cognitoUser.getSession((_, session) => {
                 if (session.isValid()) {
+                    handleTrack()
                     setSession({
                         loading: false,
                         isValid: true
