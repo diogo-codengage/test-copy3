491cb588e12ef7700f77bb64a30639060b1659bd
MarlosAugusto <marlos.gomes@codengage.com>
2019-11-29 19:11:25 -0200

    refactor(resmed): segmentTrack function

--- packages/resmed/src/Config/Segment/track.ts
+++ packages/resmed/src/Config/Segment/track.ts
@@ -1,33 +1,13 @@
-import ApolloClient from 'apollo-client'
 import { events, IEvents } from 'Config/Segment'
-import { GET_ME } from 'Apollo/User/Queries/me'
 
 export const segmentTrack = async (
-    client: ApolloClient<object>,
-    eventName: IEvents,
-    additionalAttributes: Object
+  eventName: IEvents,
+  additionalAttributes: Object
 ) => {
-    let data = {
-        ...additionalAttributes,
-        ...events[eventName].data
-    }
-    if (!additionalAttributes.hasOwnProperty('email')) {
-        const {
-            data: {
-                me: { id: userId }
-            },
-            errors
-        } = await client.query({ query: GET_ME })
-        if (!!errors) {
-            console.log('segmentTrack with ERROR:', errors)
-            throw new Error()
-        } else {
-            data = {
-                ...data,
-                'User ID': userId
-            }
-        }
-        console.log('eventName:', eventName, '- data:', data)
-        window.analytics.track(events[eventName].event, data)
-    }
+  let data = {
+    ...additionalAttributes,
+    ...events[eventName].data
+  }
+  console.log('eventName:', eventName, '- data:', data)
+  window.analytics.track(events[eventName].event, data)
 }
