c87829b6b50dc41469042c02bdebd187ff8bd3c3
MarlosAugusto <marlos.gomes@codengage.com>
2019-12-06 18:13:22 -0200

    refactor(resmed): add hasActiveSubscription and profile to IMe

--- packages/resmed/src/Apollo/User/Queries/me.ts
+++ packages/resmed/src/Apollo/User/Queries/me.ts
@@ -1,9 +1,22 @@
 import gql from 'graphql-tag'
 
+interface IProfile {
+    id: string
+    graduationStep: 'firstYear' | 'secondYear' | 'thirdYear' | 'fourthYear' | 'fifthYear' | 'sixthYear' | 'formed'
+    institutionIds: string[]
+    specialtyIds: number[]
+    testExperience: 'none' | 'one' | 'many'
+    preparatoryCourseStatus: 'inProgress' | 'completed' | 'missing'
+    userId: string
+    courseId: string
+}
+
 export interface IMe {
     id: string
     name: string
     profilePicture: string
+    hasActiveSubscription: boolean
+    profile: IProfile
 }
 
 export const GET_ME = gql`
