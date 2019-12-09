2968f753f5b9dc75c8b756db0033bfed8740bf0e
Adriano Moreira <adriano@codengage.com>
2019-12-09 11:37:13 -0300

    fix(sanarflix): lost session on reflesh

--- packages/sanarflix/src/App.tsx
+++ packages/sanarflix/src/App.tsx
@@ -30,8 +30,8 @@ const App: React.FC<RouteComponentProps> = ({ history, location }) => {
         getInstance().user.getSession((_, result) => {
             if (!result) {
                 setMe(undefined)
-            } else {
                 window.localStorage.clear();
+            } else {
                 const { pathname } = location
                 pathname.includes('auth') && history.push('/portal/inicio')
             }
