d252de64eaa576a1c2a5768331f996700fb2f81d
carol1705 <itscarolsantos@hotmail.com>
2020-01-20 18:23:50 -0300

    (hotfix/FD-914): inserting thumbnail default

--- packages/sanarflix/src/Pages/Private/Classroom/Video.tsx
+++ packages/sanarflix/src/Pages/Private/Classroom/Video.tsx
@@ -45,6 +45,7 @@ const Header = styled.div`
 `
 
 const FLXClassroomVideo = (props: RouteComponentProps<IParams>) => {
+    const thumbnailDefault = 'https://cdn-sanar.s3.amazonaws.com/thumbnail-sanarflix.png'
     const client = useApolloClient()
     const { t } = useTranslation('sanarflix')
     const {
@@ -181,7 +182,9 @@ const FLXClassroomVideo = (props: RouteComponentProps<IParams>) => {
                 const playlist = [
                     {
                         file: file && file.files.smil.url,
-                        image: resource.video.thumbnails && resource.video.thumbnails.large
+                        image: resource.video.thumbnails
+                            ? resource.video.thumbnails.large
+                            : thumbnailDefault
                     }
                 ]
 
