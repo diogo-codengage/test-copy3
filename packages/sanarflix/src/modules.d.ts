3da597d73e24bea113b5052de3bfe561937f8e00
Diogo Biz <diogo@codengage.com>
2020-01-31 08:12:02 -0300

    fix(sanarflix): add validation if exists conpass

--- packages/sanarflix/src/modules.d.ts
+++ packages/sanarflix/src/modules.d.ts
@@ -1,9 +1,10 @@
 declare module 'config' {
     global {
         interface Window {
-            analytics: any,
-            sanarflix_user_name: string | null | undefined,
-            sanarflix_user_email: string | null | undefined,
+            analytics: any
+            sanarflix_user_name: string | null | undefined
+            sanarflix_user_email: string | null | undefined
+            Conpass: any
         }
     }
 }
