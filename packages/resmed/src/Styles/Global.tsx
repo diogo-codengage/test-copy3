facc0848f4425b4e07a16ccffb141b67df3e03bf
Diogo Biz <diogo@codengage.com>
2020-02-20 10:43:11 -0300

    fix(resmed): set height auto on body

--- packages/resmed/src/Styles/Global.tsx
+++ packages/resmed/src/Styles/Global.tsx
@@ -2,19 +2,22 @@ import { createGlobalStyle } from 'styled-components'
 import { theme } from 'styled-tools'
 
 const RMGlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Nunito Sans', sans-serif !important;
-    src: url('https://fonts.googleapis.com/css?family=Nunito+Sans:700,400');
-    font-display: swap;
-  }
+  &&& {
+    @font-face {
+      font-family: 'Nunito Sans', sans-serif !important;
+      src: url('https://fonts.googleapis.com/css?family=Nunito+Sans:700,400');
+      font-display: swap;
+    }
 
-  body {
-    overflow-x: hidden;
-    background-color: ${theme('colors.grey-solid.0')};
-  }
+    body {
+      overflow-x: hidden;
+      background-color: ${theme('colors.grey-solid.0')} !important;
+      height: auto !important;
+    }
 
-  #root {
-    min-height: 100vh;
+    #root {
+      min-height: 100vh;
+    }
   }
 `
 
